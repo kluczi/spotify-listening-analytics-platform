dcb96300dd7f981ec64380fdf3ec61e621dc1220
kluczi <bartekcsgo04@gmail.com>
2026-08-27 16:55:26 +0200

    feat: add saved tracks staging model

---
+++ spotify_analytics/models/stg/stg_spotify__saved_tracks.sql
@@ -0,0 +1,18 @@
+with source as (
+    select
+        payload:track:id::string as track_id,
+        payload:track:name::string as track_name,
+        payload:track:external_urls:spotify::string as spotify_url,
+        payload:track:artists[0]:id::string as primary_artist_id,
+        payload:track:album:id::string as album_id,
+        payload:track:album:name::string as album_name,
+        payload:track:album:album_type::string as album_type,
+        payload:track:album:release_date::date as album_release_date,
+        payload:added_at::timestamp_tz as added_at,
+        fetched_at,
+        round(payload:track:duration_ms::number / 1000, 2) as duration_s,
+        payload:track:artists as artists
+    from {{ source('raw', 'saved_tracks') }}
+)
+
+select * from source
