71766008e19abd6d5a9cdb16fa9b2ebd814287c2
kluczi <bartekcsgo04@gmail.com>
2026-08-27 17:04:22 +0200

    Reapply "Feature/dbt"
    This reverts commit af58997441a3588fd488d1a3ae4959ee44007b6b.

---
+++ spotify_analytics/models/marts/int_spotify__albums.sql
@@ -0,0 +1,60 @@
+with recently_played as (
+    select
+        album_id,
+        album_name,
+        primary_artist_id,
+        album_type,
+        album_release_date,
+        fetched_at
+    from {{ ref('stg_spotify__recently_played') }}
+
+),
+
+top_tracks as (
+    select
+        album_id,
+        album_name,
+        primary_artist_id,
+        album_type,
+        album_release_date,
+        fetched_at
+    from {{ ref('stg_spotify__top_tracks') }}
+),
+
+saved_tracks as (
+    select
+        album_id,
+        album_name,
+        primary_artist_id,
+        album_type,
+        album_release_date,
+        fetched_at
+    from {{ ref('stg_spotify__saved_tracks') }}
+),
+
+unioned as (
+    select * from recently_played
+    union all
+    select * from top_tracks
+    union all
+    select * from saved_tracks
+),
+
+final as (
+    select
+        album_id,
+        album_name,
+        primary_artist_id,
+        album_type,
+        album_release_date,
+        fetched_at
+    from unioned
+    qualify row_number() over (
+        partition by album_id
+        order by
+            fetched_at desc
+    ) = 1
+
+)
+
+select * from final
